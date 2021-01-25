0fb99e71a777ba4b72a70299ec46b9c3b5f112ec
Witold Jas <jaswitold1@gmail.com>
2021-01-25 22:06:27 +0100

    leaflet map styling

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -26,8 +26,6 @@ function Maps() {
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 19,
         minZoom: 9,
         id: "mapbox/streets-v11",
